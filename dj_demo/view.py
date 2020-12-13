bf9cb73a31a75de06f3da0a4e6ba5807a97c7fa5
chris <chris@joker.li>
2020-12-13 23:58:29 +0800

    增加JSON输入框，功能留存矩阵支持输入JSON； scatter2画图细节优化

--- dj_demo/view.py
+++ dj_demo/view.py
@@ -14,28 +14,28 @@ def scatter(request):
     # data = pd.read_csv('data/demo.csv').to_json(orient='records')
     conn = hive.Connection(host='106.75.22.252', port=10008)
     data = pd.read_sql('''
-    select
-    t1.account_name,
-    t1.company_name,
-    t1.account_num,
-    t1.pay_ttl,
-    t1.recent,
-    row_number() over(partition by t1.account_name order by t1.recent) as recency_num
-from
-(
-    select
-        account_name,
-        company_name,
-        DATEDIFF(to_date(from_utc_timestamp(current_timestamp(), 'Asia/Beijing')),pay_time) as recent,
-        count(DISTINCT transaction_id) as account_num,
-        sum(pay_amount) as pay_ttl
-    from shixiaolu.test
-    group by 1,2,3
+        select
+        t1.account_name,
+        t1.company_name,
+        t1.account_num,
+        t1.pay_ttl,
+        t1.recent,
+        row_number() over(partition by t1.account_name order by t1.recent) as recency_num
+    from
+    (
+        select
+            account_name,
+            company_name,
+            DATEDIFF(to_date(from_utc_timestamp(current_timestamp(), 'Asia/Beijing')),pay_time) as recent,
+            count(DISTINCT transaction_id) as account_num,
+            sum(pay_amount) as pay_ttl
+        from shixiaolu.test
+        group by 1,2,3
+        order by pay_ttl desc
+    )t1
+    having recency_num = 1
     order by pay_ttl desc
-)t1
-having recency_num = 1
-order by pay_ttl desc
-limit 200''', conn).to_json(orient='records')
+    limit 200''', conn).to_json(orient='records')
     jsonStr = 'data=' + (data)
     context = {'jsonScript': jsonStr}
     return render(request, 'scatter.html', context)
